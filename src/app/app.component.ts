748e558e8962fa14cf3779cdca449d5c850c6de8
Lazard-Live <workbd@bk.ru>
2022-11-19 22:05:16 +0300

    переход на angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cars-app';
+
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required],
+  });
+
+  carsData = [
+    {
+      image: "1.png",
+      name: "Lamborghini Huracan Spyder",
+      engine: "полный",
+      wheel: 5.2,
+      places: 2
+    },
+    {
+      image: "2.png",
+      name: "Chevrolet Corvette",
+      engine: "полный",
+      wheel: 6.2,
+      places: 2
+    }, {
+      image: "3.png",
+      name: "Ferrari California",
+      engine: "полный",
+      wheel: 3.9,
+      places: 4
+    }, {
+      image: "4.png",
+      name: "Lamborghini Urus",
+      engine: "полный",
+      wheel: 4.0 ,
+      places: 5
+    }, {
+      image: "5.png",
+      name: "Audi R8",
+      engine: "полный",
+      wheel: 5.2,
+      places: 2
+    },
+    {
+      image: "6.png",
+      name: "Chevrolet Camaro",
+      engine: "полный",
+      wheel: 2.0,
+      places: 4
+    }
+  ]
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  goScroll(target: HTMLElement, car?: any) {
+    target.scrollIntoView({behavior: "smooth"});
+    if (car) {
+      this.priceForm.patchValue({car: car.name});
+    }
+  }
+
+  trans: any;
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+  }
+
+  bgPos: any;
+  @HostListener('document:scroll', ['$event'])
+  onScroll() {
+    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+  }
+
+  onSubmit() {
+    if (this.priceForm.valid) {
+      alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время")
+      this.priceForm.reset();
+    }
+
+  }
 }
